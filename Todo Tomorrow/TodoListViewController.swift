731f30cba90cc6b15c27d6171b9b29bf7bce474b
Ryan Vaught <rvaught2005@yahoo.com>
2018-04-09 13:43:38 -0500

    ToDo List complete with dataSource and delegate methods.

---
+++ Todo Tomorrow/TodoListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  TodoListViewController.swift
+//  Todo Tomorrow
+//
+//  Created by Ryan Vaught on 4/8/18.
+//  Copyright © 2018 Ryan Vaught. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+ 
+
+    let itemArray = ["Find Mike", "Buy Eggs", "Fill out Application"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+//        cell.messageBody.text = messageArray[indexPath.row].messageBody
+//        cell.senderUsername.text = messageArray[indexPath.row].sender
+//        cell.avatarImageView.image = UIImage(named: "egg")
+        // This might be useful in Image Oragnizer
+
+        return cell
+    }
+    // MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+ 
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        print(itemArray[indexPath.row])
+    
+    }
+}
+
